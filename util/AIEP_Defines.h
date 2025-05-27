b372451d9db97a9476677a422a154c0d40e85f73
Kwonjooeun <70623530+Kwonjooeun@users.noreply.github.com>
2025-05-27 16:59:37 +0900

    Update AIEP_Defines.h

--- util/AIEP_Defines.h
+++ util/AIEP_Defines.h
@@ -1,5 +1,5 @@
 #pragma once
-
+const int C_TRAJECTORY_SIZE = 128;		//주의!!!!!
 struct SPOINT_ENU
 {
 	double E;
@@ -29,4 +29,33 @@ struct SAIEP_WAYPOINTS
 	double arrE[15];
 	double arrN[15];
 	double arrU[15];
+};
+
+// 교전계획 결과 (ENU)
+struct SAL_MINE_EP_RESULT
+{
+	float time_to_destination;	// 부설 지점까지 총 소요 시간 [sec]
+	float RemainingTime;		// 부설 지점까지 남은 시간 [sec]
+
+	int number_of_trajectory;
+	std::array<SPOINT_ENU, C_TRAJECTORY_SIZE> trajectory;
+	std::array<float, C_TRAJECTORY_SIZE> flightTimeOfTrajectory;
+
+	int number_of_waypoint;		// 경로점 개수
+	std::array <SPOINT_M_MINE_ENU, 8> waypoints;
+	std::array<float, 8> waypointsArrivalTimes;	// 각 경로점까지의 소요시간
+
+	SPOINT_M_MINE_ENU LaunchPoint;	// 발사 지점
+	SPOINT_M_MINE_ENU DropPoint;	// 부설 지점
+
+	int idxOfNextWP;	// 다음 경로점 Index
+	float timeToNextWP;	// 다음 경로점까지 남은 시간 [sec]
+
+	bool bValidMslDRPos{ false };	// 탄 위치 유효성
+	SPOINT_ENU mslDRPos;	// 탄 위치
+
+	void reset()
+	{
+		memset(this, 0, sizeof(SAL_MINE_EP_RESULT));
+	}
 };
